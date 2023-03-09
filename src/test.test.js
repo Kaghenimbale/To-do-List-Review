52c04990fc0a7332ac2986943bda7706269439cc
Kagheni <Kaghenimbale@gmail.com>
2023-03-09 10:09:01 +0200

    Fix linters issues

--- src/test.test.js
+++ src/test.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-let todoArr = [];
+const todoArr = [];
 const input = document.createElement('input');
 input.value = 'one';
 
@@ -12,7 +12,7 @@ const dataObj = {
 };
 
 const Add = () => {
-  if(input.value.length !== 0) {
+  if (input.value.length !== 0) {
     dataObj.description = input.value;
 
     for (let i = 0; i < todoArr.length; i += 1) {
@@ -22,27 +22,27 @@ const Add = () => {
 
     localStorage.setItem('data', JSON.stringify(todoArr));
   }
-}
+};
 
 const deleteItem = (id) => {
   const indexItem = todoArr.findIndex((item) => item.index === id);
   if (indexItem > -1) {
-  todoArr.splice(indexItem, 1);
+    todoArr.splice(indexItem, 1);
 
-  todoArr.forEach((item, indexItem) => {
+    todoArr.forEach((item, indexItem) => {
       item.index = indexItem;
-  });
+    });
   }
   localStorage.setItem('data', JSON.stringify(todoArr));
-}
+};
 
 describe('Todo List', () => {
   test('Add  todo', () => {
     Add();
     expect(todoArr.length).toBe(1);
-  })
+  });
   test('delete Item', () => {
     deleteItem(1);
     expect(todoArr.length).toBe(1);
-  }) 
+  });
 });
