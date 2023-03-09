63779d36d7f3030e469de6a08e22d188166ef33f
Kagheni <Kaghenimbale@gmail.com>
2023-03-09 09:27:48 +0200

    Modify the testing

--- src/test.test.js
+++ src/test.test.js
@@ -41,9 +41,4 @@ describe('Todo List', () => {
     Add();
     expect(todoArr.length).toBe(1);
   })
-
-  test('delete Item', () => {
-    deleteItem(1)
-    expect(todoArr.length).toBe(1);
-  })
 });
